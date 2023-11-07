cbbc5fce16f24697e290851b583d669b718cf830
nicolobaldari <nicolobaldari@gmail.com>
2023-11-07 16:11:24 +0100

    chunk 1:Giocattoli

---
+++ Giocattoli.java
@@ -0,0 +1,13 @@
+ class Giocattoli {
+    private String nome;
+    private String id;
+    private double prezzo;
+    private int etaConsigliata;
+
+    public Giocattoli(String nome, String id, double prezzo, int etaConsigliata){
+        this.nome=nome;
+        this.id=id;
+        this.prezzo=prezzo;
+    }
+    
+}
